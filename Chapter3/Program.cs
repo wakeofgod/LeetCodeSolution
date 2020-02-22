b334de9ce619e0a9021c7d7594e7526fd409795d
wakeofgod <jltxwd@163.com>
2020-02-22 18:11:34 +0800

    暂存

--- Chapter3/Program.cs
+++ Chapter3/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Diagnostics;
 
 namespace Chapter3
 {
@@ -10,9 +11,44 @@ namespace Chapter3
     {
         static void Main(string[] args)
         {
+            Timing sortTime = new Timing();
+            Random rnd = new Random(100);
+            int numItems = 1000;
+            CAarry theArray = new CAarry(numItems);
+            for (int i = 0; i < numItems; i++)
+            {
+                theArray.Insert(rnd.Next(0,1000));
+            }
+            theArray.DisPlayElements();
+            sortTime.StartTime();
+            theArray.SelectionSort();
+            sortTime.StopTime();
+            Console.WriteLine();
+            Console.WriteLine($"Time for Selection sort :{sortTime.Result().TotalMilliseconds}");
+            theArray.Clear();
+            for (int i = 0; i < numItems; i++)
+            {
+                theArray.Insert(rnd.Next(0, 1000));
+            }
+            sortTime.StartTime();
+            theArray.BubbleSort();
+            sortTime.StopTime();
+            Console.WriteLine();
+            Console.WriteLine($"Time for Bubble sort :{sortTime.Result().TotalMilliseconds}");
+            theArray.Clear();
+            for (int i = 0; i < numItems; i++)
+            {
+                theArray.Insert(rnd.Next(0, 1000));
+            }
+            sortTime.StartTime();
+            theArray.InsertionSort();
+            sortTime.StopTime();
+            Console.WriteLine();
+            Console.WriteLine($"Time for Insertion sort :{sortTime.Result().TotalMilliseconds}");
+            Console.ReadLine();
         }
     }
-    class CAarry
+    public class CAarry
     {
         private int[] arr;
         private int upper;
@@ -32,7 +68,7 @@ namespace Chapter3
         {
             for (int i = 0; i <=upper; i++)
             {
-                Console.Write($"{arr[i]}");
+                Console.Write($"{arr[i]} ");
             }
         }
         public void Clear()
@@ -62,8 +98,8 @@ namespace Chapter3
                     }
                 }
                 Console.WriteLine();
-                Console.WriteLine($"显示第{upper-outer+1}次排序");
-                this.DisPlayElements();
+                //Console.WriteLine($"显示第{upper-outer+1}次排序");
+                //this.DisPlayElements();
             }
             Console.WriteLine($"共计比较次数:{total}");
         }
@@ -91,5 +127,73 @@ namespace Chapter3
             Console.WriteLine($"共计比较次数:{total}");
         }
         #endregion
+        #region 选择排序法
+        public void SelectionSort()
+        {
+            int min, temp;
+            for (int outer = 0; outer <=upper; outer++)
+            {
+                min = outer;
+                for (int inner = outer+1; inner <=upper; inner++)
+                {
+                    if (arr[inner]<arr[min])
+                    {
+                        min = inner;
+                    }
+                    temp = arr[outer];
+                    arr[outer] = arr[min];
+                    arr[min] = temp;
+                    //this.DisPlayElements();
+                }
+            }
+        }
+
+        #endregion
+        #region 插入排序法
+        public void InsertionSort()
+        {
+            int inner, temp;
+            for (int outer = 1; outer <=upper; outer++)
+            {
+                temp = arr[outer];
+                inner = outer;
+                while (inner > 0 && arr[inner - 1] >= temp)
+                {
+                    arr[inner] = arr[inner - 1];
+                    inner -= 1;
+                }
+                arr[inner] = temp;
+                //this.DisPlayElements();
+            }
+        }
+
+        #endregion
     }
+    #region 时间测试类
+    public class Timing
+    {
+        TimeSpan startingTime;
+        TimeSpan duration;
+        public Timing()
+        {
+            startingTime = new TimeSpan(0);
+            duration = new TimeSpan(0);
+        }
+        public void StopTime()
+        {
+            duration = Process.GetCurrentProcess().Threads[0].
+                UserProcessorTime.Subtract(startingTime);
+        }
+        public void StartTime()
+        {
+            GC.Collect();
+            GC.WaitForPendingFinalizers();
+            startingTime = Process.GetCurrentProcess().Threads[0].UserProcessorTime;
+        }
+        public TimeSpan Result()
+        {
+            return duration;
+        }
+    }
+    #endregion
 }
